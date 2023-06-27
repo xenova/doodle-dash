d26215f39f5248ce37f0634bf5f4815389eb098f
Joshua Lochner <admin@xenova.com>
2023-06-27 23:04:58 +0200

    Implement `getCanvasData` function

--- src/components/SketchCanvas.jsx
+++ src/components/SketchCanvas.jsx
@@ -9,6 +9,8 @@ const STOP_DRAW_EVENTS = ['mouseup', 'mouseout', 'touchend'];
 // Ensure the canvas is at least as large as the screen
 const CANVAS_SIZE = Math.max(window.screen.width, window.screen.height);
 
+const SKETCH_PADDING = 4; // number of pixels around the sketch
+
 const addEventListeners = (item, events, fn) => {
   for (let event of events) {
     item.addEventListener(event, fn);
@@ -34,9 +36,10 @@ const getPosition = (event) => {
 const SketchCanvas = forwardRef((props, ref) => {
 
   const canvasRef = useRef(null);
-  const [sketchBoundingBox, setSketchBoundingBox] = useState([0, 0, 0, 0]); // [x1, y1, x2, y2]
+  const [sketchBoundingBox, setSketchBoundingBox] = useState(null); // [x1, y1, x2, y2]
   const [isDrawing, setIsDrawing] = useState(false);
   const [brushSize, setBrushSize] = useState(8);
+
 
   // const handleBrushSizeChange = (event) => {
   //   setBrushSize(parseInt(event.target.value, 10));
@@ -72,7 +75,10 @@ const SketchCanvas = forwardRef((props, ref) => {
       context.moveTo(canvasX, canvasY);
       context.beginPath();
       setIsDrawing(true);
-      setSketchBoundingBox([canvasX, canvasY, canvasX, canvasY]);
+
+      if (sketchBoundingBox === null) {
+        setSketchBoundingBox([canvasX, canvasY, canvasX, canvasY]);
+      }
     };
 
     const draw = (event) => {
@@ -82,11 +88,13 @@ const SketchCanvas = forwardRef((props, ref) => {
       const canvasX = offsetX + paddingLeft;
       const canvasY = offsetY + paddingTop;
 
+      const brushRadius = brushSize / 2;
+
       setSketchBoundingBox([
-        Math.min(sketchBoundingBox[0], canvasX),
-        Math.min(sketchBoundingBox[1], canvasY),
-        Math.max(sketchBoundingBox[2], canvasX),
-        Math.max(sketchBoundingBox[3], canvasY),
+        Math.min(sketchBoundingBox[0], canvasX - brushRadius),
+        Math.min(sketchBoundingBox[1], canvasY - brushRadius),
+        Math.max(sketchBoundingBox[2], canvasX + brushRadius),
+        Math.max(sketchBoundingBox[3], canvasY + brushRadius),
       ]);
 
       context.lineTo(canvasX, canvasY);
@@ -116,14 +124,39 @@ const SketchCanvas = forwardRef((props, ref) => {
 
 
   const getCanvasData = () => {
-    // Perform canvas data retrieval
-    const canvasData = 'Your canvas data';
-    return canvasData;
+    if (sketchBoundingBox === null) return null;
+
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+
+    // Ensure sketch is square (and that aspect ratio is maintained)
+    let left = sketchBoundingBox[0];
+    let top = sketchBoundingBox[1];
+    let width = sketchBoundingBox[2] - sketchBoundingBox[0];
+    let height = sketchBoundingBox[3] - sketchBoundingBox[1];
+    let sketchSize = 2 * SKETCH_PADDING;
+
+    // Center the crop
+    if (width >= height) {
+      sketchSize += width;
+      top = Math.max(top - (width - height) / 2, 0);
+    } else {
+      sketchSize += height;
+      left = Math.max(left - (height - width) / 2, 0);
+    }
+
+    const imgData = context.getImageData(left - SKETCH_PADDING, top - SKETCH_PADDING, sketchSize, sketchSize);
+
+    // DEBUG:
+    // context.putImageData(imgData, 1920 / 2, 1920 / 2)
+    // context.strokeRect(1920 / 2, 1920 / 2, sketchSize, sketchSize);
+
+    return imgData;
   };
 
   // Expose the getCanvasData function to the parent component
   useImperativeHandle(ref, () => ({
-    getCanvasData: getCanvasData
+    getCanvasData: getCanvasData,
   }));
 
   return (
