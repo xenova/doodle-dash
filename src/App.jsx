73554873b2eee4a0e657d4c5c0dcc7e7f5b7379b
Joshua Lochner <admin@xenova.com>
2023-07-02 00:54:22 +0200

    Move menu to its own component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from 'react';
 import './App.css'
 import SketchCanvas from './components/SketchCanvas'
 import constants from './constants'
+import Menu from './components/Menu';
 
 function App() {
 
@@ -50,7 +51,6 @@ function App() {
         case 'ready':
           // Pipeline ready: the worker is ready to accept messages.
           setReady(true);
-          console.log('ready')
           startGame();
           break;
 
@@ -59,7 +59,6 @@ function App() {
           break;
 
         case 'result':
-
           // TODO optimize:
           const filteredResult = result.data.filter(x => !constants.BANNED_LABELS.includes(x.label));
 
@@ -155,36 +154,8 @@ function App() {
         <SketchCanvas ref={canvasRef} />
       </div>
 
-      <div className='absolute w-full h-full flex justify-center items-center flex-col px-8 text-center'>
+      <Menu gameState={gameState} onClick={handleMainClick} />
 
-        <h1
-          className='sm:text-8xl text-7xl mb-4 font-extrabold tracking-tight text-slate-900 text-center'>
-          Doodle Dash
-        </h1>
-
-        <h2
-          className='sm:text-2xl text-xl mb-3 font-semibold text-slate-900'>
-          How fast can a neural network predict your doodle?
-        </h2>
-
-        <button
-          onClick={handleMainClick}
-          type="button"
-          className={`
-          inline-flex items-center px-4 py-2 font-semibold
-          leading-6 shadow rounded-md text-white bg-indigo-500 hover:bg-indigo-400
-          transition ease-in-out duration-150 ${gameState === 'loading' ? "cursor-not-allowed" : ''}
-          `} disabled={gameState === 'loading'}>
-          {gameState === 'loading' && (
-            <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-              <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth={4}></circle>
-              <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-            </svg>
-          )}
-          {gameState === 'loading' ? 'Loading neural network...' : 'Play Game'}
-        </button>
-
-      </div>
       <div className='absolute bottom-5 text-center'>
         <h1 className="text-2xl font-bold mb-2">
           {output && `Prediction: ${output[0].label} (${output[0].score}%)`}
---
+++ src/components/Menu.jsx
@@ -0,0 +1,40 @@
+
+
+
+const Menu = (props) => {
+
+    return (
+        <div className='absolute w-full h-full flex justify-center items-center flex-col px-8 text-center'>
+            <h1
+                className='sm:text-8xl text-7xl mb-4 font-extrabold tracking-tight text-slate-900 text-center'>
+                Doodle Dash
+            </h1>
+
+            <h2
+                className='sm:text-2xl text-xl mb-3 font-semibold text-slate-900'>
+                How fast can a neural network predict your doodles?
+            </h2>
+
+            <button
+                onClick={props.onClick}
+                disabled={props.gameState !== 'menu'}
+                type="button"
+                className={`
+          inline-flex items-center px-4 py-2 font-semibold
+          leading-6 shadow rounded-md text-white bg-indigo-500 hover:bg-indigo-400
+          transition ease-in-out duration-150 ${props.gameState === 'loading' ? "cursor-not-allowed" : ''}
+          `}>
+                {props.gameState === 'loading' && (
+                    <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth={4}></circle>
+                        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
+                )}
+                {props.gameState === 'loading' ? 'Loading neural network...' : 'Play Game'}
+            </button>
+
+        </div>
+    );
+};
+
+export default Menu;
