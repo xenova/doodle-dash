118edf472073a20084adb9425e578e809ca29556
Joshua Lochner <admin@xenova.com>
2023-07-02 19:34:35 +0200

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,15 @@
 export default {
-    DEFAULT_MODEL: "mobilevit-small",
+    DEFAULT_MODEL: "quickdraw-mobilevit-small",
     DEFAULT_QUANTIZED: false,
+    BANNED_LABELS: [
+        // List of banned labels, because they are either:
+        // - Too similar to other labels
+        // - Too difficult to draw
+        // - Too difficult to understand
+        'animal migration',
+        'stitches',
+    ],
+    TARGET_FPS: 60,
+    GAME_DURATION: 10 + 0.5, // + 0.5 so it doesn't flicker (TODO: change to 60)
+    COUNTDOWN_TIMER: 3,
 };
